a15e50cee6a72f2707992b7321e3df347125108d
haeun0 <89637861+haeun0@users.noreply.github.com>
2023-05-12 17:39:18 +0900

    mysql

---
+++ mysql/쿼리 #2.sql
@@ -0,0 +1,18 @@
+SELECT NOW();
+
+/*
+	char(255) - 고정길이 (주민등록번호, 전화번호등.. 일률적인게 속도가 빠르다. 최대값 255)
+	varchar(65535) - 가변길이 
+	text
+	mediumtext
+	longtext
+
+*/
+SHOW character SET;
+
+/*
+	tinyint
+	부호가 있는 -128 ~ 127
+	부호가 없는 0 ~ 255
+
+*/
